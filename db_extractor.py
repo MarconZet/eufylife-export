48a9f9c65c6339d2a6513967ce1e462fadf0da28
MarconZet <marcin.zlakowski@gmail.com>
2020-11-06 18:30:32 +0100

    Quality of live improvements

--- db_extractor.py
+++ db_extractor.py
@@ -1,26 +1,1 @@
-import sqlite3
-
-
-def main():
-    path = "apps/com.oceanwing.smarthome/db/EufyLifeDb.db"
-
-    fields = ["id", "createtime", "weight", "bmr", "bodyfat", "bodyfatmass", "muscle", "musclemass", "bone", "bonemass",
-              "leanbodymass", "visceralfat", "water", "proteinpercentage"]
-
-    connection = sqlite3.connect(path)
-    c = connection.cursor()
-    query = "SELECT id, createtime, weight, bmr, bodyfat, bodyfatmass, muscle, musclemass, bone, bonemass, leanbodymass, visceralfat, water, proteinpercentage from bodyfathistorym"
-    response = c.execute(query).fetchall()
-
-    print(response)
-
-    csv_template = "{}," * (len(fields) - 1) + "{}\n"
-
-    file = open("body_stats.csv", "w")
-    file.write(csv_template.format(*fields))
-    for row in response:
-        file.write(csv_template.format(*row))
-
-
-if __name__ == "__main__":
-    main()
+import sqlite3def main():    path = "tmp/apps/com.oceanwing.smarthome/db/EufyLifeDb.db"    fields = ["id", "createtime", "weight", "bmr", "bodyfat", "bodyfatmass", "muscle", "musclemass", "bone", "bonemass",              "leanbodymass", "visceralfat", "water", "proteinpercentage"]    connection = sqlite3.connect(path)    c = connection.cursor()    query = "SELECT " + ("{}," * (len(fields) - 1) + "{}").format(*fields) + " from bodyfathistorym"    response = c.execute(query).fetchall()    csv_template = "{}," * (len(fields) - 1) + "{}\n"    file = open("body_stats.csv", "w")    file.write(csv_template.format(*fields))    for row in response:        file.write(csv_template.format(*row))if __name__ == "__main__":    main()
--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-tail -c +25 backup.ab | python -c "import zlib,sys;sys.stdout.write(zlib.decompress(sys.stdin.read()))" > backup.tar
-tar -xvf backup.tar
-python db_extractor.py
-rm backup.tar
+mkdir tmpcp backup.ab tmp/backup.abtail -c +25 tmp/backup.ab | python -c "import zlib,sys;sys.stdout.write(zlib.decompress(sys.stdin.read()))" > tmp/backup.tartar -xvf tmp/backup.tar --directory tmppython db_extractor.pyrm -r tmp
