6a77802530a00d853cabba54899c11b6de51e3e6
MarconZet <marcin.zlakowski@gmail.com>
2020-11-07 21:58:45 +0100

    \n fix

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-mkdir tmpcp backup.ab tmp/backup.abtail -c +25 tmp/backup.ab | python -c "import zlib,sys;sys.stdout.write(zlib.decompress(sys.stdin.read()))" > tmp/backup.tartar -xvf tmp/backup.tar --directory tmppython db_extractor.pyrm -r tmp
+mkdir tmp
+cp backup.ab tmp/backup.ab
+tail -c +25 tmp/backup.ab | python -c "import zlib,sys;sys.stdout.write(zlib.decompress(sys.stdin.read()))" > tmp/backup.tar
+tar -xvf tmp/backup.tar --directory tmp
+python db_extractor.py
+rm -r tmp
